63b02316c8a46a6b4db5b5b3d580cee5148c5fa1
Faisal-Mujtaba-Saleem <faisalmujtaba2005@gmail.com>
2024-08-22 19:13:33 -0700

    Added one comment in the code

--- djangoApi - playing with numbers/playingWithNumbers/basic_arithmetics/urls.py
+++ djangoApi - playing with numbers/playingWithNumbers/basic_arithmetics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from basic_arithmetics import views
 
+# Urls pattern has been set for the basic_arithmetics app manually
+
 urlpatterns = [
     path('total/', view=views.total, name='total'),
     path('avg/', view=views.average, name='average'),
